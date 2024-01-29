1c25512be837f02dd9a0d19125cff198b84cbf59
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-01-29 20:30:14 +0530

    Time: 127 ms (79.93%) | Memory: 71.1 MB (53.46%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,44 +1,56 @@
 class Solution {
 public:
 
-void mergeInPlace(vector<int>& arr, int s, int e){
-    int totalSize = e - s + 1;
-    // formula for finding ceil 
-    int gap = (totalSize / 2) + (totalSize % 2);
-
-    while(gap > 0){
-        int i =s, j = s+gap;
-        while(j <= e){
-            if(arr[i]>arr[j]){
-                swap(arr[i],arr[j]);
-            }
-            i++, j++;
+void merge(vector<int>& arr, vector<int>& temp, int s, int e, int mid){
+    int i = s, j =mid+1, k=s;
+    
+    //  Sorting left part and right into temp vector
+    while(i<=mid && j<=e){
+        if(arr[i] <= arr[j]){
+            temp[k++] = arr[i++];     
         }
-        gap = (gap <=1) ? 0 : (gap / 2)+(gap % 2);
+        else{
+            temp[k++] = arr[j++];
+        }
+    }
+    
+    // Copy the remaining array into the temp 
+    while(i<=mid){
+        temp[k++] = arr[i++];
+    }
+    while(j<=e){
+        temp[k++] = arr[j++];
+    }
+    
+    // Copy the temp array into the main array
+    while(s<=e){
+        arr[s] = temp[s];
+        ++s;
     }
 }
 
-void mergeSort(vector<int>& arr, int s, int e){
+void mergeSort(vector<int>& arr,vector<int>& temp, int s, int e){
     //! Base Case : Single element / invalid array
     if(s>=e){
         return;
     }
     int mid = s + (e-s)/2;
     //TODO 1 : left part ko sort krdo
-    mergeSort(arr, s,mid); 
+    mergeSort(arr,temp, s,mid); 
 
     // TODO 2 : Right Part ko sort krod 
-    mergeSort(arr, mid+1, e);
+    mergeSort(arr,temp, mid+1, e);
 
     // TODO 3 : merge 2 sorted arrays 
-    mergeInPlace(arr, s, e);
+    merge(arr, temp, s, e, mid);
 }
 
 vector<int> sortArray(vector<int>& nums) {
     int n = nums.size();
     int s =0;
     int e =n-1;
-    mergeSort(nums, s,e);  
+    vector<int> temp(n,0);
+    mergeSort(nums, temp, s,e);  
     return nums;
     }
 };

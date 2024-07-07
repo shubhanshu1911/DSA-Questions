045d4c4a780efbbd581232263f1a1b71ebaee39d
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-07-08 00:46:11 +0530

    Time: 12 ms (8.36%) | Memory: 12.9 MB (72.44%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        
+        // Step 1 : Clone A -> A'
+        Node* it = head; // iterate over old head
+        while (it){
+            Node* clonedNode = new Node(it -> val);
+            clonedNode -> next = it -> next;
+            it -> next = clonedNode;
+            it = it -> next -> next;
+        }
+
+        // Step 2 : Assign random Links of A' with the help of A
+        it = head;
+        while(it){
+            Node* clonedNode = it -> next;
+            clonedNode -> random = it -> random ? it -> random -> next : nullptr;
+            it = it -> next -> next;
+        }
+
+        // Step 3 : Detach A' from A 
+        it = head;
+        Node* clonedHead = it -> next;
+        while(it){
+            Node* clonedNode = it -> next;
+            it -> next = it -> next -> next;
+            if(clonedNode -> next){
+                clonedNode -> next = clonedNode -> next -> next;
+            }
+            it = it -> next;
+        }
+        return clonedHead;
+    }
+};

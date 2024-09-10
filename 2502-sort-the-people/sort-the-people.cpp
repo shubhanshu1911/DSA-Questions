bdfcb12676e1d7f4814cfd61576e0fd7225effed
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-09-10 15:18:11 +0100

    Time: 28 ms (58.29%) | Memory: 30.4 MB (11.10%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,54 +1,19 @@
 class Solution {
 public:
-    void insertSorted(stack< pair<string,int> > &s, pair<string,int> target){
-        // base case 
-        if(s.empty()){
-            s.push(target);
-            return;
-        }
-        if(s.top().second <= target.second){
-            s.push(target);
-            return;
-        }
-
-        // 1 case 
-        pair<string,int> topElement = s.top();
-        s.pop();
-        // rec call
-        insertSorted(s, target);
-        // BT
-        s.push(topElement);
-    }
-
-    void sortStack(stack< pair<string,int> > &s){
-        // base case 
-        if(s.empty()){
-            return;
-        }
-
-        pair<string,int> topElement = s.top();
-        s.pop();
-
-        sortStack(s);
-
-        insertSorted(s, topElement);
-    }
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         vector<string> ans;
-        stack< pair<string,int> > s;
+        vector<pair<int,string>>v;
+        
+        for(int i=0;i<heights.size();i++){
+            v.push_back({heights[i],names[i]});
+        }
+        sort(v.rbegin(),v.rend());
 
-        for(int i=0; i<heights.size(); i++){
-            s.push(make_pair(names[i], heights[i]));
+
+        for(int i=0; i<v.size(); i++){
+            ans.push_back(v[i].second);
         }
 
-        sortStack(s);
-
-        while(!s.empty()){
-            string currElement = s.top().first;
-            s.pop();
-            ans.push_back(currElement);
-        }
-        // reverse(ans.begin(), ans.end());
         return ans;
     }
 };

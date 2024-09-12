f8eaf887a5a0e7eaa1954caeb461bfef29088062
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-09-12 22:46:51 +0100

    Time: 0 ms (100.00%) | Memory: 7.9 MB (89.15%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack <string> st;
+        for(auto ch : s){
+            if(ch == ']'){
+                string stringToRepeat = "";
+                // jo char h vo digit ke equal nhi h
+                while(!st.empty() && !isdigit(st.top()[0])){
+                    // string top = ;
+                    stringToRepeat += st.top() == "[" ? "" : st.top(); // cancatinate all the char into the string
+                    st.pop();
+                }
+
+                string numericTimes = "";
+                // jo char h vo digit ke equal h
+                while(!st.empty() && isdigit(st.top()[0])){
+                    numericTimes += st.top();
+                    st.pop();
+                }
+                reverse(numericTimes.begin(), numericTimes.end());
+                int n = stoi(numericTimes);
+
+                // final decoding
+                string currentDecode = "";
+                while(n--){
+                    currentDecode += stringToRepeat;
+                }
+                st.push(currentDecode);
+
+            }
+            // ingore this '[' 
+            else{
+                string temp (1,ch); // string initialise with single char "ch"
+                st.push(temp);
+            }
+        }
+
+        string ans;
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

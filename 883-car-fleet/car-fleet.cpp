82ac8d469d2a13df2606fbe7855a31fc55db7cf7
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-09-14 13:28:55 +0100

    Time: 136 ms (45.82%) | Memory: 80.8 MB (85.63%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -3,27 +3,27 @@ public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
         vector<pair<int, double>> cars(n); // Pair of position and time to target
+        // int fleet;
+        stack<double> st;
 
         // Create a pair of {position, time to reach target}
         for (int i = 0; i < n; ++i) {
             cars[i] = {position[i], (double)(target - position[i]) / speed[i]};
         }
 
-        // Sort cars by position in descending order (closer to target first)
-        sort(cars.rbegin(), cars.rend());
+        // Sort cars by position in increasing order (closer to target first)
+        sort(cars.begin(), cars.end());
 
-        int fleets = 0;
-        double lastTime = 0.0;
-
-        // Traverse from the car closest to the target to the farthest
-        for (int i = 0; i < n; ++i) {
-            // If the current car takes more time than the last car's fleet, it forms a new fleet
-            if (cars[i].second > lastTime) {
-                fleets++;
-                lastTime = cars[i].second; // Update the last fleet's time
+        for(int j=0; j<cars.size(); j++){
+            // cout << cars[j].first << "," << cars[j].second << " ";
+            double currTime = cars[j].second;
+            // Insert into stack
+            while(!st.empty() && currTime >= st.top()){
+                st.pop();
             }
+            st.push(currTime);
         }
 
-        return fleets;
+        return st.size();
     }
 };

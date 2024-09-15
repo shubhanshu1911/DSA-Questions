18bad7225d36a1e0892a9c4500796ba9bda0bc0d
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-09-15 19:00:06 +0100

    Time: 15 ms (9.68%) | Memory: 20.3 MB (95.39%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -15,16 +15,17 @@ public:
         // base case 
         if(root == 0) return true;
 
-        // root -> data range me h toh 
-        if(root -> val > lb && root -> val < ub){
-            bool leftAns = solve(root -> left, lb, root -> val);
-            bool rightAns = solve(root -> right, root-> val, ub);
-            return leftAns && rightAns;
-        }
-        else{
+        // root -> data range me nhi h toh return false 
+        if(root -> val <= lb || root -> val >= ub){
             return false;
         }
+
+        // Root range h to check left and rigght 
+        bool leftAns = solve(root -> left, lb, root -> val);
+        bool rightAns = solve(root -> right, root-> val, ub);
+        return leftAns && rightAns;
     }
+
     bool isValidBST(TreeNode* root) {
         long long int lowerBound = LONG_MIN;
         long long int upperBound = LONG_MAX;

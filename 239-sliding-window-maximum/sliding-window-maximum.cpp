a2d7ab3d04ab03de783da9b037c5c0e8088a9fc8
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-09-30 07:25:58 +0100

    Time: 189 ms (29.35%) | Memory: 152.6 MB (21.04%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       deque <int> dq;
-       vector <int> ans;
-
-        // first k size ki window ko process krna h
-        for(int i=0; i<k; i++){
-            // current element se chote element ko window se remove krdo
-            while(!dq.empty() && nums[i] >= nums[dq.back()]){
-                dq.pop_back();
-            }
-
-            // inserting index of current element to check out of the window
-            dq.push_back(i);
-
+        vector<int> ans;
+        priority_queue<pair<int, int>> maxHeap;
+        
+        // Add the first 'k' elements to the heap
+        for (int i = 0; i < k; i++) {
+            maxHeap.push({nums[i], i});
         }
-
-        // ans ko store krlo 
-        ans.push_back(nums[dq.front()]);
-
-        // remaining windows ko process krlo 
-        for(int i=k; i<nums.size(); i++){
-            // out of window remove krro
-            if(!dq.empty() && i-dq.front() >= k){
-                dq.pop_front();
+        ans.push_back(maxHeap.top().first);
+        
+        // Process the remaining elements in the array
+        for (int i = k; i < nums.size(); i++) {
+            maxHeap.push({nums[i], i});
+            
+            // Remove elements from the heap that are out of the current window
+            // The current window is from index [i - k + 1] to i
+            while (maxHeap.top().second <= i - k) {
+                maxHeap.pop();
             }
-
-            // aab firse current element ke liye chote element remove krro
-            while(!dq.empty() && nums[i] >= nums[dq.back()]){
-                dq.pop_back();
-            }
-
-            // inserting index of current element to check out of the window
-            dq.push_back(i);
-
-            // ans ko store krlo 
-            ans.push_back(nums[dq.front()]);
+            
+            ans.push_back(maxHeap.top().first);
         }
-
+        
         return ans;
     }
 };

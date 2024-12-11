e2ea572cc7fe468c5b435e4d181e1b8bf00f01cc
Shubhanshu Khare <100808882+shubhanshu1911@users.noreply.github.com>
2024-12-11 23:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (5.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -21,16 +21,38 @@ public:
         if(dp[n] != -1){
             return dp[n];
         }
-        
+
         // Step 2 : Replace the functions and ans with dp[n]
         dp[n] = topDownSolve(n-1, dp) + topDownSolve(n-2, dp);
         return dp[n];
     }
 
+    int bottomUpSolve(int n){
+        // Step 1 : Create dp array 
+        vector<int> dp(n+1, -1);
+        // Step 2 : Obverse base case in above sitution
+        dp[0]=0;
+        if(n == 0) 
+            return dp[0];
+
+        dp[1] = 1;
+        if(n == 1) 
+            return dp[1];
+
+        // Step 3 : 
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
     int fib(int n) {
         // step 1 : Create dp array
-        vector<int> dp(n+1, -1);
-        int ans = topDownSolve(n, dp);
+        // vector<int> dp(n+1, -1);
+        // int ans = topDownSolve(n, dp);
+        // return ans;
+
+        int ans = bottomUpSolve(n);
         return ans;
         
     }
